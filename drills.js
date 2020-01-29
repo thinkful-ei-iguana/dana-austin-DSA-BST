a26c9845507ca4acab90fd71b7a35384d701498a
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-28 19:23:34 -0800

    wrote a solution for balanced search tree

--- drills.js
+++ drills.js
@@ -31,26 +31,42 @@ function main() {
   }
   console.log(tree);
   console.log(tree2);
-  console.log('break-line')
-  console.log(findHeight(tree))
-  console.log(thirdLargest(tree))
+  console.log('break-line');
+  console.log(findHeight(tree));
+  console.log(thirdLargest(tree));
+  console.log(isBalanced(tree));
 }
 
 function findHeight(Bst, counter = 1) {
   if (Bst.left && Bst.right) {
-    return Math.max(...[findHeight(Bst.left, counter + 1), findHeight(Bst.right, counter + 1)])
+    return Math.max(
+      ...[
+        findHeight(
+          Bst.left,
+          counter + 1
+        ),
+        findHeight(
+          Bst.right,
+          counter + 1
+        )
+      ]
+    );
   } else if (Bst.left) {
-    return findHeight(Bst.left, counter + 1)
+    return findHeight(
+      Bst.left,
+      counter + 1
+    );
   } else if (Bst.right) {
-    return findHeight(Bst.right, counter + 1)
+    return findHeight(
+      Bst.right,
+      counter + 1
+    );
   } else {
     return counter;
   }
 }
 
-function isBst() {
-
-}
+function isBst() {}
 
 // function thirdLargest(Bst, counter = 1) {
 //   if(!Bst.parent) {
@@ -66,19 +82,156 @@ function isBst() {
 //   }
 // }
 
-function thirdLargest(Bst, counter = 1) {
-  if(!Bst.parent) {
-    return [{density: thirdLargest(Bst.left).reduce((a,b) => a.density + b.density) + counter + thirdLargest(Bst.right).reduce((a,b) => a.density + b.density), key: Bst.key},...thirdLargest(Bst.left, counter), ...thirdLargest(Bst.right, counter)].sort((nodeA, nodeB) => nodeA.height - nodeB.height)
+/**
+ * this actually finds the third densest node as we misread
+ * the problem to mean that the biggest node is root
+ * because it has the most leaves whereas the third
+ * biggest node would have the  third most leaves...
+ *
+ */
+function thirdLargest(
+  Bst,
+  counter = 1
+) {
+  if (!Bst.parent) {
+    return [
+      {
+        density:
+          thirdLargest(Bst.left).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ) +
+          counter +
+          thirdLargest(
+            Bst.right
+          ).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ),
+        key: Bst.key
+      },
+      ...thirdLargest(
+        Bst.left,
+        counter
+      ),
+      ...thirdLargest(
+        Bst.right,
+        counter
+      )
+    ].sort(
+      (nodeA, nodeB) =>
+        nodeB.density - nodeA.density
+    )[2].key;
   } else if (Bst.left && Bst.right) {
-    return [{density: thirdLargest(Bst.left).reduce((a,b) => a.density + b.density) + counter + thirdLargest(Bst.right).reduce((a,b) => a.density + b.density), key: Bst.key},...thirdLargest(Bst.left, counter), ...thirdLargest(Bst.right, counter)]
+    return [
+      {
+        density:
+          thirdLargest(Bst.left).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ) +
+          counter +
+          thirdLargest(
+            Bst.right
+          ).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ),
+        key: Bst.key
+      },
+      ...thirdLargest(
+        Bst.left,
+        counter
+      ),
+      ...thirdLargest(
+        Bst.right,
+        counter
+      )
+    ];
   } else if (Bst.left) {
-    return [{density: thirdLargest(Bst.left).reduce((a,b) => a.density + b.density) + counter, key: Bst.key},...thirdLargest(Bst.left, counter)]
+    return [
+      {
+        density:
+          thirdLargest(Bst.left).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ) + counter,
+        key: Bst.key
+      },
+      ...thirdLargest(Bst.left, counter)
+    ];
   } else if (Bst.right) {
-    return [{density: thirdLargest(Bst.right).reduce((a,b) => a.density + b.density) + counter, key: Bst.key},...thirdLargest(Bst.right, counter)]
+    return [
+      {
+        density:
+          thirdLargest(
+            Bst.right
+          ).reduce(
+            (total, b) =>
+              total + Number(b.density),
+            0
+          ) + counter,
+        key: Bst.key
+      },
+      ...thirdLargest(
+        Bst.right,
+        counter
+      )
+    ];
   } else {
-    return [{density: counter, key: Bst.key}]
+    return [
+      { density: counter, key: Bst.key }
+    ];
   }
 }
 
+function isBalanced(bst, distance = 0) {
+  if (bst.parent === null) {
+    let leaves = [
+      ...isBalanced(bst.left, distance),
+      ...isBalanced(bst.right, distance)
+    ];
+
+    for (let leaf of leaves) {
+      for (let leaf2 of leaves) {
+        if (
+          Math.abs(leaf - leaf2) > 1
+        ) {
+          return false;
+        }
+      }
+    }
+  }
+  if (bst.left && bst.right) {
+    return [
+      ...isBalanced(
+        bst.left,
+        ++distance
+      ),
+      ...isBalanced(bst.right, distance)
+    ];
+  } else if (bst.left) {
+    return [
+      ...isBalanced(
+        bst.left,
+        ++distance
+      )
+    ];
+  } else if (bst.right) {
+    return [
+      ...isBalanced(
+        bst.right,
+        ++distance
+      )
+    ];
+  } else {
+    return [++distance];
+  }
+}
 
 main();
